0e8fa551f3dd8ea0c80f2571903d59c9f19b122c
Yiorgis Gozadinos <ggozadinos@gmail.com>
2026-07-15 12:33:53 +0300

    Restore hotpotqa evaluation dataset

--- evaluations/evaluations/datasets/__init__.py
+++ evaluations/evaluations/datasets/__init__.py
@@ -1,5 +1,6 @@
 from evaluations.config import DatasetSpec
 
+from .hotpotqa import HOTPOTQA_SPEC
 from .open_rag_bench import (
     ORB_MULTIMODAL_NEMOTRON_SPEC,
     ORB_MULTIMODAL_SPEC,
@@ -12,6 +13,7 @@ DATASETS: dict[str, DatasetSpec] = {
     spec.key: spec
     for spec in (
         WIX_SPEC,
+        HOTPOTQA_SPEC,
         ORB_TEXT_SPEC,
         ORB_MULTIMODAL_SPEC,
         ORB_MULTIMODAL_NEMOTRON_SPEC,
